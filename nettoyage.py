57ef78c7ba989f56c34fbc10029b42a1bcee81e9
AaronRmj <aaronrmjk@gmail.com>
2026-09-06 11:39:00 +0300

    remove accents from french word input or dataset

--- nettoyage.py
+++ nettoyage.py
@@ -3,12 +3,28 @@ import string
 
 #Mettre en miniscule, enlever ponctuation, enlever espace
 def nettoyer(texte):
+    
     if texte is None:
-        return
+        return ""
+    
+    synonyme_syllabe = {
+
+        "à": "a",
+        "ù": "u",
+        "é": "e",
+        "è": "e",
+        "ê": "e",
+        "ç": "c"
+
+    }
+
+    #on retourne cle, valeur avec la clé qui sera remplacé par la valeur
+    for syllabe_accentuee, syllabe_normale in synonyme_syllabe.items():
+        texte = texte.replace(syllabe_accentuee, syllabe_normale)
+
+    
     texte = texte.lower().strip()
     for symbole in string.punctuation:
         texte = texte.replace(symbole, " ")
     texte = " ".join(texte.split())
     return texte
-
-
